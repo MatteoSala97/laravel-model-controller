1902cda77a02f61dc00be7b171d34a46d2037e98
Matteo Sala <matteo111997@gmail.com>
2024-03-14 18:55:43 +0100

    style fixes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,24 +1,42 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LARAVEL MODEL CONTROLLER</title>
 
     @vite('resources/js/app.js')
-    
+
 </head>
 <body>
-    <div class="container">
 
-        <ul>
+    <h1 class="text-center mt-5">
+        Movies
+    </h1>
+
+    <div class="container mt-5">
+        <div class="row">
             @foreach ( $movie as $element)
-            <li>
-                <h1>{{$element["title"]}}</h1>
-            </li>
+            <div class="col-4">
+                <div class="card mt-5">
+                    <div class="card-body">
+                        <h4 class="card-title">Titolo: {{$element["title"]}}</h4>
+                        <h5 class="card-title">Titolo originale: {{$element["original_title"]}}</h5>
+                        <p class="card-text">Data di uscita: {{$element["date"]}}</p>
+                        <p class="card-text">Voto: {{$element["vote"]}}</p>
+                    </div>
+                </div>
+            </div>
             @endforeach
-        </ul>
-
+        </div>
     </div>
 </body>
+<style>
+    .card{
+        height: 250px;
+        text-align: center;
+        line-height: 50px;
+    }
+    
+</style>
 </html>
